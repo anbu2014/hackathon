2eeadff5cb22a7a7ad01b4155bf629af3efa128a
anbu2014 <90215292+anbu2014@users.noreply.github.com>
2021-12-28 11:39:09 +0530

    comments added

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+
+/* 
+script for Cat API
+
+author Anbarasan G
+*/
+
+
 let main_URL = "https://cataas.com/api/cats";
 let json_arry=[];
+
+/*
+IIFE function 
+ Used the async/await.
+Used try catch to handle errors.
+Used fetch() to get the data from
+
+@return array of JSON data 
+*/
 ( async()=>
 {
     try
@@ -22,6 +39,12 @@ let json_arry=[];
 let table_head = document.getElementById("table_head");
 let err_msg = document.getElementById("err_msg");
 
+/* 
+Create a tr and th on the thead and append the data on thead tag
+@para 
+@return
+
+*/
 function tableHead(){
      table=`<tr>
      <th scope="col">sno</th>
@@ -31,30 +54,49 @@ function tableHead(){
    </tr>`
    table_head.innerHTML=table;
 }
+
+
 let h6 = document.getElementById("h6")
 let all_cat_list = document.getElementById("all_cat_list")
 
+/*
+H6 tag is dynamically changed 
+when mouse hover to the all_cat_list button 
+
+*/
 all_cat_list.addEventListener("mouseover", ()=>{
   h6.innerHTML="This might take few seconds please wait"
   
 })
 
+/*
+On click of all_cat_list button 
 
+diaplay all the details of the API data 
+H6 tag is dynamically changed 
+when json data is displayed on the table 
+
+*/
 all_cat_list.addEventListener("click",()=>{
  
     displayDetails(json_arry)
     h6.innerHTML="click to view the cat list"
 })
 
+/*
+displayDetails method get the data from the json_arry
+display all the detail in the table 
 
+@para json_arry
+*/
 
 let displayDetails =(json_arry)=>{
     let table_body = document.getElementById("table_body")
     let tablerow=""
     tableHead();
-    var i =0;
+    var i =1;
     for(let arr of json_arry){
-      if( i<270){
+      if( i<=270){
         tablerow +=`
         <tr value="${arr.id}" class="img_2">
         <td>${i}</td>
@@ -79,19 +121,37 @@ let displayDetails =(json_arry)=>{
     
     }
 
+    
+  
 
 let search_txt_Box = document.getElementById("search_Box")
 let search = document.getElementById("search")
+/*
+H6 tag is dynamically changed 
+when mouse hover to the search_txt_Box field
 
+*/
 search_txt_Box.addEventListener("mouseover",()=>{
  
   h6.innerHTML="click to view the cat list"
 })
+
+/*
+H6 tag is dynamically changed 
+when mouse hover to the search button 
+
+*/
 search.addEventListener("mouseover",()=>{
  
   h6.innerHTML="click to view the cat list"
 })
 
+/*
+on click of search button 
+get the value on the search text 
+compare with the JSON data and display the details
+
+*/
 
 search.addEventListener("click" , ()=>{
        let enteredValue = search_txt_Box.value;
@@ -134,6 +194,14 @@ search.addEventListener("click" , ()=>{
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var model_content = document.getElementById("res")
+
+/*
+function popup when the View button is clicked 
+div tag is appened to the model_content div
+display css property is changed to block
+on click of span 
+display css property is changed to none
+*/
 function popup(r) {
   var i = r.parentNode.parentNode.rowIndex;
  let ans= document.getElementsByClassName("img_2")[i-1].getAttribute("value")
@@ -153,8 +221,3 @@ let span = document.getElementById("span")
  
 }
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
